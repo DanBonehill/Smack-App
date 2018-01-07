ae87bf2ba733fb50a9bd7de715428792961274d5
DanBonehill <DanBonehill@me.com>
2018-01-07 14:56:24 +0000

    Create and complete LoginVC

---
+++ Smack-App/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack-App
+//
+//  Created by Daniel Bonehill on 07/01/2018.
+//  Copyright © 2018 DanBonehill. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack-App/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack-App
+//
+//  Created by Daniel Bonehill on 07/01/2018.
+//  Copyright © 2018 DanBonehill. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
