21e58cfe7eeeb24f6eec73ea97fb57ef2dc20600
DanBonehill <DanBonehill@me.com>
2018-01-07 21:53:49 +0000

    Add loginUser functionality

--- Smack-App/Services/AuthService.swift
+++ Smack-App/Services/AuthService.swift
@@ -8,13 +8,14 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class AuthService {
     static let instance = AuthService()
     
     let defaults = UserDefaults.standard
     
-    var isLoggedIN: Bool {
+    var isLoggedIn: Bool {
         get {
             return defaults.bool(forKey: LOGGED_IN_KEY)
         }
@@ -44,17 +45,41 @@ class AuthService {
     func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
         let lowerCaseEmail = email.lowercased()
         
-        let header = [
-            "Content-Type": "application/json; charset=utf-8"
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
         ]
+        
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseString { (response) in
+            if response.result.error == nil {
+                completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+    
+    func loginUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        let lowerCaseEmail = email.lowercased()
         
         let body: [String: Any] = [
             "email": lowerCaseEmail,
             "password": password
         ]
         
-        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+        Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (response) in
             if response.result.error == nil {
+                guard let data = response.data else { return }
+                do {
+                    let json = try JSON(data: data)
+                    
+                    self.userEmail = json["user"].stringValue
+                    self.authToken = json["token"].stringValue
+                } catch {
+                    debugPrint(error)
+                }
+                self.isLoggedIn = true
                 completion(true)
             } else {
                 completion(false)
