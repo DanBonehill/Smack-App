73a1b99f8b815ae71c4c75b62ce2f532b3ac6df1
DanBonehill <DanBonehill@me.com>
2018-02-09 20:21:08 +0000

    Display messages

---
+++ Smack-App/View/MessageCell.swift
@@ -0,0 +1,28 @@
+//
+//  MessageCell.swift
+//  Smack-App
+//
+//  Created by Daniel Bonehill on 09/02/2018.
+//  Copyright © 2018 DanBonehill. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var userImg: CircleImage!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var timestampLbl: UILabel!
+    @IBOutlet weak var messageBodyLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func configureCell(message: Message) {
+        messageBodyLbl.text = message.message
+        userNameLbl.text = message.userName
+        userImg.image = UIImage(named: message.userAvatar)
+        userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+    }
+}
